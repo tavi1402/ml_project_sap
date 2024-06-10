f1311f0e1cfc810de0581ca9b0bcc7181afb437f
Tavishi <100408675+tavi1402@users.noreply.github.com>
2024-06-10 19:55:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))  # Changed to 8000
+    port = int(os.environ.get('PORT', 5001))  # Changed to 8000
     app.run(host='0.0.0.0', port=port)
